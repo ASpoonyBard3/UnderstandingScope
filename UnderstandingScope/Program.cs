ff33dd2a5f18049f0a560dcbf48f39fbdca0e8da
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-30 11:41:14 +0100

    add private and public scoped methods that are siblings that allow sharing of data across the main function, then added if statement that was in the inner most code block and was not accessible to the other methods and fails to call.

--- UnderstandingScope/Program.cs
+++ UnderstandingScope/Program.cs
@@ -8,14 +8,39 @@ namespace UnderstandingScope
 {
     class Program
     {
+        private static string k = "";
+
+
         static void Main(string[] args)
         {
+
+            string j = "";
+
             for (int i = 0; i < 10; i++)
             {
+                j = i.ToString();
+                k = j.ToString();
                 Console.WriteLine(i);
 
+                if (i == 9);
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l);
+
             }
+            //Console.WriteLine(i);
+            Console.WriteLine("Out of the for: " + j);
+            Console.WriteLine("Out of the for: " + k);
+
+            HelperMethod();
+
             Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("The value of k, from the helperMethod():" + k);
         }
     }
 }
